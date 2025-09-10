57bb2a0ca268091a45a5272c834eee31603b9468
Erik Abrahamsson <erik.abrahamsson16@hotmail.com>
2025-09-10 11:19:45 +0200

    first commit

---
+++ MyFirstProgram/MyFirstProgram/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace MyFirstProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Erik Abrahamsson, NET25
+            
+            int number = 10;         //User decides what the int "number" should be
+
+            if (number > 10)        //The if-statement checks if the number is greater than 10 and if it is, it writes a specific comment
+            {
+                Console.WriteLine("Talet är stort!");
+            }
+            else                    //If the number is or less than 10, it writes a different comment
+            {
+                Console.WriteLine("Oj, lågt tal!");
+            }
+
+            Console.WriteLine("Vad heter du?");
+
+            string name = Console.ReadLine();           //The user writes in what the string "name" should be
+
+            Console.WriteLine("Hej " + name + "!");     
+
+
+            
+            
+            for (int numList = 0; numList < number + 1; numList++) //The loop lists the number starting from 0 to the number that the user set
+            {
+                Console.WriteLine(numList);
+            }
+        }
+    }
+}
